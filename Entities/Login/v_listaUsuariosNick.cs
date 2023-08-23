1b7776a76d176ea1aaff539494144878654243b6
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-08-23 16:17:29 -0300

    Entities de la vista V_listaUsuariosNick

---
+++ Entities/Login/v_listaUsuariosNick.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRVM2019.Entities.Login
+{
+    public class v_listaUsuariosNick
+    {
+        [Key]
+        public int IDUsuario { get; set; }
+        public string Nick { get; set; }
+        public string Correo { get; set; }
+    }
+}
