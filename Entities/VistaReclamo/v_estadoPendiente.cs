384e8e5473a3c4fe7b6dad48ad1357b034a7f9fe
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-10-18 08:50:58 -0300

    Entidad v_estadoPendiente

---
+++ Entities/VistaReclamo/v_estadoPendiente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRVM2019.Entities.VistaReclamo
+{
+    public class v_estadoPendiente
+    {
+        [Key]
+        public int IDEstado { get; set; }
+
+        public string Nombre { get; set; }
+        public int ID_TipoEstado { get; set; }
+        public int IDTipoEstado { get; set; }
+
+        public string nombreTipoEstado { get; set; }
+    }
+}
