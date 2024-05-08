4319ef2149bed88dd9b806509fa5d9dec39eaf93
Dandrea Omar <omar_dandrea97@hotmail.com>
2024-05-08 02:31:35 -0300

    Se creo la tabla de la vista V_ReclamosEnElTiempo

---
+++ Entities/Estadistica/V_ReclamosEnElTiempo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRVM2019.Entities.Estadistica
+{
+    public class V_ReclamosEnElTiempo
+    {
+        [Key]
+        public int CantidadReclamo { get; set; }
+
+        public string Hora { get; set; }
+        public string TipoHora { get; set; }
+        public string NombreMes {get; set;}
+        public int Anio { get; set; }
+        public int numeroDIa { get; set; }
+        public int idUsuario { get; set; }
+
+    }
+}
