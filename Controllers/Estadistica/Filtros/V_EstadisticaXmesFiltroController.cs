ee7e4804872a80d96ec6634a9c78e25b95537e77
Dandrea Omar <omar_dandrea97@hotmail.com>
2024-05-12 23:12:24 -0300

    V_EstadisticaXmesFiltroControlles creado y listo para el uso del cliente - falta el administrador

---
+++ Controllers/Estadistica/Filtros/V_EstadisticaXmesFiltroController.cs
@@ -0,0 +1,150 @@
+﻿using ApiRVM2019.Contexts;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiRVM2019.Entities;
+using Microsoft.EntityFrameworkCore;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRVM2019.Controllers.Estadistica.Filtros
+{
+    [EnableCors("All")]
+    [Route("[controller]")]
+    [ApiController]
+    public class V_EstadisticaXmesFiltroController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public V_EstadisticaXmesFiltroController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<V_EstadisticaXmesFiltroController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<V_EstadisticaXmesFiltroController>/5
+        [HttpGet("{idUsuario}/{idRol}/{anio}/{idLocalidad}")]
+        public IActionResult GetDatos(int idUsuario, int idRol, string anio, int idLocalidad)
+        {
+            //Cuando se filtra estos parametros
+            //localidad 
+            //https://localhost:44363/V_EstadisticaXmesFiltro/2/3/2024/1
+            //Administrador
+            if (idRol == 1)
+            {
+                if ((anio == null) || anio == "")
+                {
+                    //todos los reclamos - historicos de esa localidad
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+                }
+                else
+                {
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.anio == anio
+
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+                }
+            }
+            else
+            {
+                //Usuario
+                //https://localhost:44363/V_EstadisticaXmesFiltro/2/3/2024/1
+                if ((anio == null) || anio == "")
+                {
+                    //todos los reclamos del usuario para filtrar solamente por localidad
+                    // año vacio
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.ID_Localidad == idLocalidad
+                                && reclamosXmes.IDUsuario == idUsuario
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+                }
+                else
+                {
+                    //cuando quiere filtrar por anio y localidad
+                   
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.anio == anio && reclamosXmes.IDUsuario == idUsuario
+                                && reclamosXmes.ID_Localidad == idLocalidad
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+                }
+            }
+            //https://localhost:44363/EstPorcentajeCalleXLocalidad/2
+
+
+        }
+
+        // POST api/<V_EstadisticaXmesFiltroController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<V_EstadisticaXmesFiltroController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<V_EstadisticaXmesFiltroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
