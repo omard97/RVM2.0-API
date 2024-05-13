b0b9938b5f12b6895c10e616d90c8762ff6a805d
Dandrea Omar <omar_dandrea97@hotmail.com>
2024-05-12 23:12:42 -0300

    V_ReclamosEnElTiempoFiltroController creado y listo para el uso del cliente - falta el administrador

---
+++ Controllers/Estadistica/Filtros/V_ReclamosEnElTiempoFiltroController.cs
@@ -0,0 +1,96 @@
+﻿using ApiRVM2019.Contexts;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiRVM2019.Entities;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRVM2019.Controllers.Estadistica.Filtros
+{
+    [EnableCors("All")]
+    [Route("[controller]")]
+    [ApiController]
+    public class V_ReclamosEnElTiempoFiltroController : ControllerBase
+    {
+
+        private readonly AppDbContext context;
+        public V_ReclamosEnElTiempoFiltroController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<V_ReclamosEnElTiempoFiltroController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<V_ReclamosEnElTiempoFiltroController>/5
+        [HttpGet("{idRol}/{idUsuario}/{mes}/{anio}/{idLocalidad}")]
+        public IActionResult GetreclamosTiempo(int idRol, int idUsuario, int mes, int anio, int idLocalidad)
+        {
+            //Cuando utilicen los filtros se ejecuta esto
+            //https://localhost:44363/V_ReclamosEnElTiempoFiltro/3/2/2/2024/1
+            if (idRol == 1)
+            {
+                var data = from vista in context.V_ReclamosEnElTiempo
+                           group vista by new { vista.Hora, vista.TipoHora } into g
+                           select new
+                           {
+                               name = g.Key.Hora,
+                               value = g.Sum(x => x.CantidadReclamo),
+                           };
+                if (data == null)
+                {
+                    return NotFound();
+                }
+                return Ok(data);
+            }
+            else
+            {
+                if (idRol == 3)
+                {
+                    var data = from vista in context.V_ReclamosEnElTiempo
+                               where vista.idUsuario == idUsuario && vista.Anio <= anio
+                               && vista.ID_Localidad == idLocalidad
+                               group vista by new { vista.Hora, vista.TipoHora } into g
+                               select new
+                               {
+                                   name = g.Key.Hora,
+                                   value = g.Sum(x => x.CantidadReclamo),
+                               };
+                    if (data == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(data);
+                }
+
+                return NotFound();
+            }
+        }
+
+        // POST api/<V_ReclamosEnElTiempoFiltroController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<V_ReclamosEnElTiempoFiltroController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<V_ReclamosEnElTiempoFiltroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
