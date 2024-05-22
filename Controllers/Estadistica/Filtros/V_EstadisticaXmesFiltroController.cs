6010450941370dbb62ea39fb129f3a17f162b158
Dandrea Omar <omar_dandrea97@hotmail.com>
2024-05-21 22:55:45 -0300

    V_EstadisticaXMesFiltro funcionalidad de filtros y validaciones

--- Controllers/Estadistica/Filtros/V_EstadisticaXmesFiltroController.cs
+++ Controllers/Estadistica/Filtros/V_EstadisticaXmesFiltroController.cs
@@ -40,11 +40,13 @@ namespace ApiRVM2019.Controllers.Estadistica.Filtros
             //Administrador
             if (idRol == 1)
             {
-                if ((anio == null) || anio == "")
+
+                //Obligatoriamente tiene que buscar por localidad y año ARREGLAR
+                if ( anio == "0" && idLocalidad!=0)
                 {
-                    //todos los reclamos - historicos de esa localidad
+                    //todos los reclamos - historicos de esa localidad sin  año
                     var dato = (from reclamosXmes in context.V_EstadisticaXmes
-                                
+                                where reclamosXmes.ID_Localidad == idLocalidad
                                 group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
                                 select new
                                 {
@@ -57,11 +59,49 @@ namespace ApiRVM2019.Controllers.Estadistica.Filtros
                         return NotFound();
                     }
                     return Ok(dato);
+                }else if (anio!="0" && idLocalidad!= 0)
+                {
+                    //cuando busca por localidad y anio
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.ID_Localidad == idLocalidad && reclamosXmes.anio==anio
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+                }else if (anio != "0" && idLocalidad == 0)
+                {
+                    //cuando solo busca por anio sin localidad
+                    // es decir todos lo de ese año
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.anio == anio
+
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
                 }
                 else
                 {
+                    //no ingresa ningun dato de filtro, busca para todos
                     var dato = (from reclamosXmes in context.V_EstadisticaXmes
-                                where reclamosXmes.anio == anio
+                                
 
                                 group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
                                 select new
@@ -82,10 +122,53 @@ namespace ApiRVM2019.Controllers.Estadistica.Filtros
             {
                 //Usuario
                 //https://localhost:44363/V_EstadisticaXmesFiltro/2/3/2024/1
-                if ((anio == null) || anio == "")
+
+                if (idLocalidad != 0 && anio != "0")
                 {
-                    //todos los reclamos del usuario para filtrar solamente por localidad
-                    // año vacio
+                    //busque por localidad y anio
+
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.ID_Localidad == idLocalidad
+                                && reclamosXmes.IDUsuario == idUsuario && reclamosXmes.anio == anio
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+
+
+
+                }
+                else if (idLocalidad == 0 && anio != "0")
+                {
+                    //busco por anio y de cualquier localidad
+                    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                                where reclamosXmes.anio == anio && reclamosXmes.IDUsuario == idUsuario 
+                                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                                select new
+                                {
+                                    name = m.Key.NombreMes,
+                                    value = m.Sum(x => x.Cantidad),
+                                    numero = m.Key.numeroMes
+                                }).OrderBy(x => x.numero);
+                    if (dato == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(dato);
+
+                }
+                else if (idLocalidad != 0 && anio == "0")
+                {
+
+                    //busco por localidad de cualquier anio
                     var dato = (from reclamosXmes in context.V_EstadisticaXmes
                                 where reclamosXmes.ID_Localidad == idLocalidad
                                 && reclamosXmes.IDUsuario == idUsuario
@@ -101,14 +184,13 @@ namespace ApiRVM2019.Controllers.Estadistica.Filtros
                         return NotFound();
                     }
                     return Ok(dato);
+
                 }
                 else
                 {
-                    //cuando quiere filtrar por anio y localidad
-                   
+                    //busco por todo sin ingresar anio y localidad, es decir un historico de todas las localidades y de todos los años
                     var dato = (from reclamosXmes in context.V_EstadisticaXmes
-                                where reclamosXmes.anio == anio && reclamosXmes.IDUsuario == idUsuario
-                                && reclamosXmes.ID_Localidad == idLocalidad
+                                where  reclamosXmes.IDUsuario == idUsuario 
                                 group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
                                 select new
                                 {
@@ -116,13 +198,56 @@ namespace ApiRVM2019.Controllers.Estadistica.Filtros
                                     value = m.Sum(x => x.Cantidad),
                                     numero = m.Key.numeroMes
                                 }).OrderBy(x => x.numero);
-
                     if (dato == null)
                     {
                         return NotFound();
                     }
                     return Ok(dato);
                 }
+
+
+
+                //if ((anio == null) || anio == "")
+                //{
+                //    //todos los reclamos del usuario para filtrar solamente por localidad
+                //    // año vacio
+                //    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                //                where reclamosXmes.ID_Localidad == idLocalidad
+                //                && reclamosXmes.IDUsuario == idUsuario
+                //                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                //                select new
+                //                {
+                //                    name = m.Key.NombreMes,
+                //                    value = m.Sum(x => x.Cantidad),
+                //                    numero = m.Key.numeroMes
+                //                }).OrderBy(x => x.numero);
+                //    if (dato == null)
+                //    {
+                //        return NotFound();
+                //    }
+                //    return Ok(dato);
+                //}
+                //else
+                //{
+                //    //cuando quiere filtrar por anio y localidad
+                   
+                //    var dato = (from reclamosXmes in context.V_EstadisticaXmes
+                //                where reclamosXmes.anio == anio && reclamosXmes.IDUsuario == idUsuario
+                //                && reclamosXmes.ID_Localidad == idLocalidad
+                //                group reclamosXmes by new { reclamosXmes.NombreMes, reclamosXmes.numeroMes } into m
+                //                select new
+                //                {
+                //                    name = m.Key.NombreMes,
+                //                    value = m.Sum(x => x.Cantidad),
+                //                    numero = m.Key.numeroMes
+                //                }).OrderBy(x => x.numero);
+
+                //    if (dato == null)
+                //    {
+                //        return NotFound();
+                //    }
+                //    return Ok(dato);
+                //}
             }
             //https://localhost:44363/EstPorcentajeCalleXLocalidad/2
 
